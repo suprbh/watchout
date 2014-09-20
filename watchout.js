0aa693e66ea33177d1a0886b2b1cba961e1eaab0
suprbh <supb27@yahoo.com>
2014-09-20 12:29:40 -0700

    not using tween for collision check

--- watchout.js
+++ watchout.js
@@ -3,7 +3,7 @@
 var gameOptions = {
   height: 450,
   width: 700,
-  nEnemies: 30,
+  nEnemies: 1,
   padding: 20
 };
 
@@ -18,7 +18,9 @@ var createEnemies = function(gameOptions){
       id: element,
       x: axes.x(Math.random()*100),
       y: axes.y(Math.random()*100),
-      radius: 10
+      radius: 10,
+      oldX: 0,
+      oldY: 0
     };
   });
   return enemies;
@@ -54,6 +56,7 @@ var svg = d3.select("body").append("svg")
 // }
 var enemyUpdate = function (data) {
 
+  // debugger;
   // DATA JOIN
   // Join new data with old elements, if any.
   var circleEnemy = svg.selectAll("circle.enemy")
@@ -63,15 +66,20 @@ var enemyUpdate = function (data) {
   // Create new elements as needed.
   circleEnemy.enter().append("circle")
       .attr("class", "enemy")
-      .attr("r", "10");
+      .attr("r", "10")
+      .attr('cx', function(d){return d.x})
+      .attr('cy', function(d){return d.y});
 
   // ENTER + UPDATE
   // Appending to the enter selection expands the update selection to include
   // entering elements; so, operations on the update selection after appending to
   // the enter selection will apply to both entering and updating nodes.
-  circleEnemy.transition().duration(1000)
-    .attr("cx", function(d) {return d.x})
-    .attr("cy", function(d) {return d.y});
+  // startPosX = circleEnemy.attr("cx")
+  // startPosY = circleEnemy.attr("cy")
+  //debugger;
+  circleEnemy.transition().duration(3000)
+    // .attr("cx", function(d) {return d.x})
+    // .attr("cy", function(d) {return d.y});
     // .tween('collision', function(endData){
     //   //interpolate x and y coordinates
     //   //invoke collisionCheck
@@ -81,8 +89,9 @@ var enemyUpdate = function (data) {
     //   };
     //   tweenCollisionCheck(enemy,player);
     // });
-    collisionCheck(data,player);
-
+     .tween('collision', tweenCollisionCheck);
+    //collisionCheck(data,player);
+    shuffleEnemies(data);
 
   // EXIT
   // Remove old elements as needed.
@@ -103,13 +112,53 @@ var collision = function(){
   scoreboard.currentScore = 0;
 };
 
-var tweenCollisionCheck = function(enemy,player){
-  var distance;
-  distance = Math.sqrt((Math.pow((player.x - enemy.x),2)) + (Math.pow((player.y - enemy.y),2)));
-  if ( 10*(player.radius + enemy.radius) > distance ) {
-    console.log(distance);
-    collision();
-  }
+var tweenCollisionCheck = function(d){
+  debugger;
+  var enemy = d3.select(this);
+
+  var startPos = {
+    x: parseFloat(enemy.attr('cx')),
+    y: parseFloat(enemy.attr('cy')),
+    r: parseFloat(enemy.attr('r'))
+  };
+
+  var endPos = {
+    x: d.x,
+    y: d.y
+  };
+  // console.log('enemy', d.id);
+  // console.log('start', startPos, 'end', endPos);
+  // var counter = 0;
+  // debugger;
+  var i = d3.interpolate(startPos.x, endPos.x);
+  var j = d3.interpolate(startPos.y, endPos.y);
+
+  return function(t){
+    debugger;
+    console.log('enemy', d.id);
+    console.log('startPos, endPos inside return fn', startPos, endPos);
+    // counter++;
+    // if (counter > 10) {
+    //    debugger;
+    // }
+
+    var distance;
+    distance = Math.sqrt((Math.pow((player.x - i(t)),2)) + (Math.pow((player.y - j(t)),2)));
+    if ((player.radius + d.radius) > distance ) {
+      console.log(distance);
+      collision();
+    }
+
+    enemyNextPos = {
+      x: i(t),
+      y: j(t)
+    };
+
+
+    enemy.attr('cx', enemyNextPos.x);
+    enemy.attr('cy', enemyNextPos.y);
+  };
+
 };
 
 var collisionCheck = function(enemies,player){
@@ -129,6 +178,8 @@ var collisionCheck = function(enemies,player){
 
 var shuffleEnemies = function(enemies){
   _.each(enemies, function(enemy, index){
+    enemy.oldX = enemy.x;
+    enemy.oldY = enemy.y;
     enemy.x = axes.x(Math.random()*100);
     enemy.y = axes.y(Math.random()*100);
   });
@@ -141,6 +192,7 @@ var dragMove = function(d){
   d3.select(this)
     .attr('cx', d.x)
     .attr('cy', d.y);
+    // collisionCheck(enemies,d)
 };
 
 var drag = d3.behavior.drag()
@@ -153,8 +205,8 @@ var player = {
   radius: 10
 };
 
-var watchout = function(){
 var enemies = createEnemies(gameOptions);
+var watchout = function(){
 // {"x": axes.x(50), "y": axes.y(50)}
   var circlePlayer = svg.selectAll('circle.player')
     .data([player])
@@ -171,7 +223,7 @@ var enemies = createEnemies(gameOptions);
   //   // collisionCheck(enemies,player);
   // },10);
   setInterval(function(){
-    enemyUpdate(shuffleEnemies(enemies));
+    enemyUpdate(enemies); // shuffleEnemies(enemies)
     scoreboard.currentScore++;
     d3.selectAll('div.scoreboard div')
     .select('span')
